293774fbd8b54ea797b8a71ec81953fa874e135f
Bishal <bohorabishal20@gmail.com>
2024-04-03 13:59:05 +0545

    new commit

---
+++ src/Keyboard.tsx
@@ -0,0 +1,26 @@
+import styles from './Keyboard.module.css'
+
+const KEYS = [ "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+
+type KeyboardProps = {
+    activeLetters: string[]
+    inactiveLetters: string[]
+    addGuessedLetter: (letter: string) => void
+}
+
+export function Keyboard({activeLetters, inactiveLetters, addGuessedLetter}: KeyboardProps){
+    return(
+        <div style={{
+            display:'grid', 
+            gridTemplateColumns:'repeat(auto-fit, minmax(75px,1fr)',
+            gap:'.5rem'
+        }}>
+            {KEYS.map(key => {
+                return (
+                    <button className={`${styles.btn} `} key={key}>{key}</button>
+                )
+            })}
+        </div>
+    )
+
+}
